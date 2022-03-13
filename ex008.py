f6e6ae291b46d7a756e593018300a8a14c8db9f2
Erick Melo <mmelo.erick@gmail.com>
2022-03-13 19:26:23 -0300

    Conversor de medidas

---
+++ ex008.py
@@ -0,0 +1,8 @@
+medida = float(input('Uma distância em metros: '))
+print('{}km'.format(medida/1000))
+print('{}hm'.format(medida/100))
+print('{}dam'.format(medida/10))
+print('{}m'.format(medida))
+print('{}dm'.format(medida*10))
+print('{}cm'.format(medida*100))
+print('{}mm'.format(medida*1000))
