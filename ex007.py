b44234fd603e8875eed772a1f1d1feb94253ffb8
Erick Melo <mmelo.erick@gmail.com>
2022-03-13 19:15:11 -0300

    Media Aritmetica

--- ex007.py
+++ ex007.py
@@ -1,3 +1,6 @@
 nota1 = float(input('Informe a primeira nota: '))
 nota2 = float(input('Informe a segunda nota: '))
 
+media = (nota1 + nota2) / 2
+
+print('A media das notas {} e {} é de {}.'.format(nota1, nota2, media))
