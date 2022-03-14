dbb89e99a3113d8fef72736f8d5ea05a378eb7e3
Erick Melo <mmelo.erick@gmail.com>
2022-03-13 22:17:54 -0300

    Reajuste Salarial

--- ex013.py
+++ ex013.py
@@ -1,6 +1,7 @@
 #Reajuste salarial com 15% 
 sal = float(input('Informe o salário: R$'))
 
-novosal = (sal * 85) / 100
+porc = (sal * 15) / 100
+novosal = sal + porc
 
-print('O salário de R${:.2f} com o desconto passa a ser de R${:.2f}'.format(sal, novosal))
+print('O salário de R${:.2f} com o aumento de 15% passa a ser de R${:.2f}'.format(sal, novosal))
