f2cb220bc78aa1b8639d806c16c4f4103ef3dbf0
Erick Melo <mmelo.erick@gmail.com>
2022-03-13 22:15:01 -0300

    Calculando desconto

--- ex012.py
+++ ex012.py
@@ -2,6 +2,7 @@
 preco = float(input('Informe o preço do produto: '))
 desconto = int(input('Informe a porcentagem de desconto: '))
 
-novopreco = (preco * desconto) / 100
+desc = (preco * desconto) / 100
+novopreco = preco - desc
 
 print('O preço é de R${:.2f}, como o desconto de {}% o preço passa a ser de R${:.2f}.'.format(preco, desconto, novopreco))
