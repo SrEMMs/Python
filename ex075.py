e673706c659295caa27daf184145feae2208606c
SrEMMs <mmelo.erick@gmail.com>
2023-10-25 09:49:04 -0300

    Análise de dados em uma Tupla

---
+++ ex075.py
@@ -0,0 +1,14 @@
+num = (int(input('Digite um número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite mais um número: ')),
+       int(input('Digite o último número: ')))
+print(f'Você digitou os valores {num}')
+print(f'O valor 9 apareceu {num.count(9)} vezes')
+if 3 in num:
+    print(f'O valor 3 apareceu na {num.index(3)+1}ª posição')
+else:
+    print('O valor 3 não aparece em nenhuma posição')
+print('Os valores pares digitados foram: ', end='')
+for i in num:
+    if i % 2 == 0:
+        print(i, end=' ')
