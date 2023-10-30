61000e6b8b6789041f7a6ec3a392373305b0d328
SrEMMs <mmelo.erick@gmail.com>
2023-10-30 14:51:31 -0300

    Unindo dicionários e listas

---
+++ ex094.py
@@ -0,0 +1,42 @@
+pessoa = dict()
+galera = list()
+
+soma = 0
+media = 0
+
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo [M/F]: ')).strip().upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        op = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+        if op in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if op == 'N':
+        break
+
+print('-=' * 30)
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas!')
+media = soma / len(galera)
+print(f'B) A média de idade é de {media:5.2f} anos.')
+print('C) As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] in 'F':
+        print(f'{p["nome"]} ', end='')
+print()
+print('D) Lista das pessoas que estão acima da média: ')
+for p in galera:
+    if p['idade'] >= media:
+        print('     ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< Encerrado >>')
